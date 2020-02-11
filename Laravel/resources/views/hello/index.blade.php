b68f9ffbd6419cd08de52fe82b20070df181d65e
vagrant <vagrant@homestead>
2020-02-11 06:20:52 +0000

    6

--- Laravel/resources/views/hello/index.blade.php
+++ Laravel/resources/views/hello/index.blade.php
@@ -4,11 +4,14 @@
 <title>Index</title>
 </head>
 <body>
-
+<style>
+th {background-color:red; padding:10px;}
+td {background-color:#eee; padding:10px;}
+</style>
 <h1>Hello/Index(modified 9)</h1>
 
 <p>
-{{$msg}}
+{!! $msg !!}
 </p>
 @foreach($data as $item)
 <li>{{$item}}</li>
