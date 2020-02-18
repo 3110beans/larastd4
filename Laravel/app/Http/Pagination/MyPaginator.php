12e36208ec603cf7dca5978948431b7a8587b16b
3110beans <vagrant@homestead>
2020-02-18 23:48:05 +0000

    18

--- Laravel/app/Http/Pagination/MyPaginator.php
+++ Laravel/app/Http/Pagination/MyPaginator.php
@@ -3,6 +3,7 @@ namespace App\Http\Pagination;
 use Illuminate\Contracts\Pagination\Paginator;
 
 class MyPaginator{
+
 	private $paginator;
 
 	public function __construct(Paginator $paginator){
@@ -10,7 +11,14 @@ class MyPaginator{
 	}
 
 	public function link(){
-		$prev = $this->paginator->currentPage() 1 ? ' disabled : ';
+		$prev = $this->paginator->currentPage() 1 ? ' disabled ' : '';
+		$next = $this->paginator->count() ? ' disabled' : '';
+		$result = '<ul class="pagination" role="navigation">';
+		$result .= '<li class="page-item' . $prev . '"><a class="page-link" href=' . $this->paginator->previousPageUrl() . '">前のページ</a></li>';
+		$result .= '<li class="page-item disabled"><a class="page-link">' . $this->paginator->currentPageUrl() . '</a></li>';
+		$result .= '<li class="page-item' . $next . '"><a class="page-link" href=' . $this->paginator->nextPageUrl() . '">次のページ</a></li>';
+		$result .= '</ul>';
+		return $result;
 
 	}
 }
